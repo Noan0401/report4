125bf946d86b22b003851c8c69f09df9c39f730b
Karin OGawA <e185744@ie.u-ryukyu.ac.jp>
2018-11-13 13:14:45 +0900

    ステップ3: APIリファレンスを参照してみよう。
    変数strに、文字列"百二十三"を代入
    Integer.parseInt(String s)メソッドを用いて、int型変数valueに代入する

--- src/jp/ac/uryukyu/ie/e185744/Main.java
+++ src/jp/ac/uryukyu/ie/e185744/Main.java
@@ -2,15 +2,9 @@ package jp.ac.uryukyu.ie.e185744;
 
 public class Main {
     public static void main(String[] args){
+        
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
 
-        try {
-            //処理したいコード
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            //IOExceptionが起きたときの処理
-            System.out.println("NullPointerExceptionが発生");
-            System.out.println(e.getMessage());
-        }
     }
 }
